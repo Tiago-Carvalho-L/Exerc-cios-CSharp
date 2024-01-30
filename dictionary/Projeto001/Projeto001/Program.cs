74ea1e527f6df02c1ff3b8cab942055005432871
Tiago Lopes <lopestiago127@gmail.com>
2024-01-30 10:04:59 -0300

    Exercício de dictionary adicionado

---
+++ dictionary/Projeto001/Projeto001/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> palavras = new Dictionary<string, int>();
+
+            Console.WriteLine("Escreva uma frase SEM VÍRGULAS: ");
+            string[] frase = Console.ReadLine().Split(' ');
+            Console.WriteLine();
+
+            for(int i = 0; i < frase.Length; i++)
+            {
+                if (palavras.ContainsKey(frase[i]))
+                {
+                    palavras[frase[i]] += 1; 
+                }
+                else
+                {
+                    palavras[frase[i]] = 1;
+                }
+            }
+
+            foreach(KeyValuePair<string, int> palavra in palavras)
+            {
+                Console.WriteLine($"{palavra.Key}: {palavra.Value}");
+            }
+        }
+    }
+}
