475184d5278425d5d090c7a76c3e2b90335337a9
Tiago Lopes <lopestiago127@gmail.com>
2024-01-31 11:25:31 -0300

    Adicionado exercício de extensions methods

---
+++ extensions methods/Projeto001/Projeto001/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projeto001.Extensions
+{
+    static class ListExtensions
+    {
+        public static List<int> FiltrarPares(this List<int> list)
+        {
+            List<int> ints = new List<int>();
+
+            foreach (int item in list)
+            {
+                if (item % 2 == 0)
+                {
+                    ints.Add(item);
+                }
+            }
+
+            return ints;
+        }
+    }
+}
---
+++ extensions methods/Projeto001/Projeto001/Program.cs
@@ -0,0 +1,30 @@
+﻿using Projeto001.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace ProjetoUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listadInteiros = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            List<int> listaFiltrada = listadInteiros.FiltrarPares();
+
+            Console.WriteLine("LISTA ORIGINAL:");
+            foreach (int item in listadInteiros)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("LISTA FILTRADA:");
+            foreach (int i in listaFiltrada)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();   
+        }
+    }
+}
